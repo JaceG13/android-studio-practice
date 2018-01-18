345bbdf8862ea53638a2cdf2dad53e07630654e6
Jason Gooden <jaygooden@hotmail.com>
2018-01-18 11:25:09 -0500

    Created button to redirect from main page to second page

--- app/src/main/java/com/example/jaygo/helloworld/MainActivity.java
+++ app/src/main/java/com/example/jaygo/helloworld/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.jaygo.helloworld;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                goToSecondActivity();
+
+            }
+
+        });
+    }
+
+    private void goToSecondActivity() {
+
+        Intent intent = new Intent(this, SecondActivity.class);
+
+        startActivity(intent);
+
     }
 }
 
