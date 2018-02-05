8727747234e66897a008f1ff69de699472a934f5
Jason Gooden <jaygooden@hotmail.com>
2018-02-05 17:03:19 -0500

    Created second page

--- app/src/main/java/com/example/jaygo/helloworld/MainActivity.java
+++ app/src/main/java/com/example/jaygo/helloworld/MainActivity.java
@@ -4,36 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.jaygo.helloworld.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button = findViewById(R.id.button1);
-
-        button.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-
-            public void onClick(View v) {
-
-                goToSecondActivity();
-
-            }
-
-        });
     }
 
-    private void goToSecondActivity() {
-
-        Intent intent = new Intent(this, SecondActivity.class);
-
+    //Called when user taps Send button
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
-
     }
 }
 
